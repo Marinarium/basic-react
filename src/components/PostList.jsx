85894fac775cdd75902c4bafa29efa80f31d04ef
marinarium <schwmk@gmail.com>
2023-09-19 16:36:30 +0300

    add animation

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import PostItem from './PostItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
-const PostList = ({posts, title, remove}) => {
+const PostList = ({ posts, title, remove }) => {
   return (
     <div className="posts">
       <h2 className="posts__title">{title}</h2>
       {
         posts.length !== 0
-          ? posts.map((post, i) => <PostItem key={post.id} id={i+1} post={post} remove={remove}/>)
+          ? <TransitionGroup>
+              {posts.map((post, i) => <CSSTransition
+                key={post.id}
+                timeout={500}
+                classNames="animated"
+              >
+                <PostItem id={i + 1} post={post} remove={remove}/>
+              </CSSTransition>)}
+            </TransitionGroup>
           : <p>Как-так?! Ничего нет?! Добавь карточку ;)</p>
       }
     </div>
